b9d78c33cc4495344a7f0ab69c05601c81db139c
ShabnamZ <shabnam.ziyovuddinova@gmail.com>
2019-08-05 16:15:53 -0400

    Models and half of the methods in service  layer for Income is added

---
+++ BudgApp.Models/Income/IncomeDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgApp.Models.Income
+{
+    public class IncomeDetails
+    {
+
+        public int IncomeID { get; set; }
+        [Required]
+        public float IncomeAmount { get; set; }
+        [Required]
+        public DateTime IncomeDate { get; set; }
+    }
+}
--- BudgApp.Service/IncomeService.cs
+++ BudgApp.Service/IncomeService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BudgApp.Data;
+using BudgApp.Data.Models;
+using BudgApp.Models.Income;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,44 @@ namespace BudgApp.Service
 {
     public class IncomeService
     {
+        private readonly Guid _userID;
+
+        public IncomeService(Guid userID)
+        {
+            _userID = userID;
+        }
+
+        public bool CreateIncome(IncomeCreate model)
+        {
+            var entity = new Income()
+            {
+                AccountID = _userID,
+                IncomeAmount = model.IncomeAmount,
+                IncomeDate = model.IncomeDate
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Incomes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<IncomeListItem> GetIncome()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Incomes
+                  .Where(e => e.AccountID == _userID)
+                 .Select(e => new IncomeListItem
+                 {
+                     IncomeID = e.IncomeID,
+                     IncomeAmount = e.IncomeAmount,
+                     IncomeDate = e.IncomeDate
+
+                 });
+                return query.ToArray();
+            }
+        }
     }
 }

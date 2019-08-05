ed06196fa038c25b3f70e52b7163dc214bcfb59d
ShabnamZ <shabnam.ziyovuddinova@gmail.com>
2019-08-05 16:38:25 -0400

    Income Service layer is added

--- BudgApp.Service/IncomeService.cs
+++ BudgApp.Service/IncomeService.cs
@@ -18,7 +18,7 @@ namespace BudgApp.Service
             _userID = userID;
         }
 
-        public bool CreateIncome(IncomeCreate model)
+        public bool CreateIncome(IncomeCreate model) //creates income
         {
             var entity = new Income()
             {
@@ -27,14 +27,14 @@ namespace BudgApp.Service
                 IncomeDate = model.IncomeDate
             };
 
-            using (var ctx = new ApplicationDbContext())
+            using (var ctx = new ApplicationDbContext()) 
             {
                 ctx.Incomes.Add(entity);
                 return ctx.SaveChanges() == 1;
             }
         }
 
-        public IEnumerable<IncomeListItem> GetIncome()
+        public IEnumerable<IncomeListItem> GetIncome() // gets all incomes
         {
             using (var ctx = new ApplicationDbContext())
             {
@@ -48,6 +48,67 @@ namespace BudgApp.Service
 
                  });
                 return query.ToArray();
+            }
+        }
+
+        public IEnumerable<IncomeListItem> GetIncomeByMonth(DateTime monthKey) // Gets the incomes of a specified month
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Incomes
+                    .Where(e => e.IncomeDate.Month == monthKey.Month && e.AccountID == _userID).Select(e => new IncomeListItem
+                    {
+                        IncomeID = e.IncomeID,
+                        IncomeAmount = e.IncomeAmount,
+                        IncomeDate = e.IncomeDate,
+                    });
+                return entity.ToArray();
+            }
+
+        }
+        public IncomeDetails GetIncomeByID(int id) // gets income based on ID
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Incomes
+                    .Single(e => e.IncomeID == id && e.AccountID == _userID);
+                return new IncomeDetails
+                {
+                    IncomeID = entity.IncomeID,
+                    IncomeAmount = entity.IncomeAmount,
+                    IncomeDate = entity.IncomeDate
+                };
+            }
+        }
+
+        public bool UpdateIncome(IncomeEdit model) // updates existing income
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Incomes
+                    .Single(e => e.IncomeID == model.IncomeID && e.AccountID == _userID);
+
+                entity.IncomeID = model.IncomeID;
+                entity.IncomeAmount = model.IncomeAmount;
+                entity.IncomeDate = model.IncomeDate;
+
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+
+        public bool DeleteIncome(int incomeID) // deletes selected income
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Incomes
+               .Single(e => e.IncomeID == incomeID && e.AccountID == _userID);
+
+                ctx.Incomes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
             }
         }
     }

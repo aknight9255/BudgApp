b4c0e78ced24140883b0b78df90f7f5efa56bd75
aknight9255 <amanda.knight55@gmail.com>
2019-08-19 13:40:40 -0400

    Added Admin. Mason Waz Here

--- BudgApp.API/Startup.cs
+++ BudgApp.API/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using BudgApp.Data.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 
 [assembly: OwinStartup(typeof(BudgApp.API.Startup))]
@@ -14,6 +18,42 @@ namespace BudgApp.API
         {
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        //in this method we create default User roles and Admin user for login 
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //In startup iam creating first Admin role and creating a default Admin User 
+            if (!roleManager.RoleExists("Admin"))
+            {
+                //first we create admin role
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.Email = "admin@admin.admin";
+
+                string userPWD = "Admin1!";
+
+                var checkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (checkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+
+            }
         }
     }
 }

7e5fda73d50b50e2b888508492da499f9e4b1ac5
aknight9255 <amanda.knight55@gmail.com>
2019-08-19 15:26:31 -0400

    user admin part 2

--- BudgApp.API/Controllers/AccountController.cs
+++ BudgApp.API/Controllers/AccountController.cs
@@ -59,10 +59,16 @@ namespace BudgApp.API.Controllers
         public UserInfoViewModel GetUserInfo()
         {
             ExternalLoginData externalLogin = ExternalLoginData.FromIdentity(User.Identity as ClaimsIdentity);
+            string role = "";
+            if (User.IsInRole("Admin"))
+            {
+                role = "Admin";
+            }
 
             return new UserInfoViewModel
             {
                 Email = User.Identity.GetUserName(),
+                Role = role,
                 HasRegistered = externalLogin == null,
                 LoginProvider = externalLogin != null ? externalLogin.LoginProvider : null
             };
